7eaa3fd15227dc8c96d987a2c716bdf1c67b48a0
Lanisha <lanishaanusri28@gmail.com>
2022-05-06 11:11:37 +0800

    .

--- hakim.php
+++ hakim.php
@@ -4,49 +4,6 @@
         header("Location:./login.php");
     include("sambungan.php");
 
-    // create tables if dont have
-    // urusetia
-    try {
-        $sql = "SELECT * FROM urusetia";
-        $result = mysqli_query($sambungan,$sql);
-    } catch (exception $e) {
-        $sql = <<<HEREDOC
-        CREATE TABLE urusetia (
-            id INT(10) NOT NULL AUTO_INCREMENT,
-            nama_pengguna VARCHAR(30) NOT NULL,
-            kata_laluan VARCHAR(15) NOT NULL
-        )
-        HEREDOC;
-        $result = mysqli_query($sambungan,$sql);
-    }
-    // hakim
-    try {
-        $sql = "SELECT * FROM hakim";
-        $result = mysqli_query($sambungan,$sql);
-    } catch (exception $e) {
-        $sql = <<<HEREDOC
-        CREATE TABLE hakim (
-            id INT(10) NOT NULL AUTO_INCREMENT,
-            nama VARCHAR(30) NOT NULL
-        )
-        HEREDOC;
-        $result = mysqli_query($sambungan,$sql);
-    }
-    // peserta
-    try {
-        $sql = "SELECT * FROM peserta";
-        $result = mysqli_query($sambungan,$sql);
-    } catch (exception $e) {
-        $sql = <<<HEREDOC
-        CREATE TABLE peserta (
-            id INT(10) NOT NULL AUTO_INCREMENT,
-            no_kp VARCHAR(15) NOT NULL,
-            nama VARCHAR(30) NOT NULL
-        )
-        HEREDOC;
-        $result = mysqli_query($sambungan,$sql);
-    }
-
     // Get all hakim
     $hakim = [];
     $sql = "SELECT * FROM hakim";
